d75d31f9529ee2d7fe1bde2dcca0ab235d80d6cc
Annelie <annelieberner@gmail.com>
2021-09-07 14:45:35 +0200

    random symbols
    but have to massage the others to match

--- particles/cacb-js/draft_10.js
+++ particles/cacb-js/draft_10.js
@@ -6,8 +6,11 @@ var tagTable = [];
 var svg, g, node, text, link;
 var liveLinks = [];
 var liveNodes = [];
+var liveTopics = [];
 
-var radius = 1;
+var radius = 30;
+var symWidth = radius;
+var symHeight = radius;
 
 var dataset;
 var liveData = [];
@@ -15,7 +18,7 @@ var simulation;
 
 var maxStrength = 0.25;
 //hacky way to get the vis started well
-var whichNum= -2;
+var whichNum= 0;
 
 const makeRequest = async () => {
   try {
@@ -29,44 +32,41 @@ const makeRequest = async () => {
 }
 
 var topicNodes = [];
-var freq = [];
+var types = [];
 const getNodes = async(dataset)=>{
     for (var i = 0; i<dataset.nodes.length; i++){
         nodes.push(dataset.nodes[i])
-        freq.push(dataset.nodes[i].frequency)
-        if(dataset.nodes[i].answer==-1){
-            topicNodes.push(dataset.nodes[i])
-        }
+        // if(dataset.nodes[i].first==0){
+            topicNodes.push(dataset.nodes[i]) //it looks cool when they are all topic nodes
+        // }
     }
 
     for (var i = 0; i<dataset.tags.length; i++){
         tagTable.push(dataset.tags[i]);
+        types.push(dataset.tags[i].type);
     }
     return nodes;
 }
 
-var radScale = d3.scaleLinear()
-    .range([radius*2, radius*50])
+
+var poScale = d3.scaleOrdinal()
+
 const processPrep = async(dataset, nodes) => {
     width = window.innerWidth*.99;
     height = window.innerHeight*.99;
 
-    var poScale = d3.scaleOrdinal()
-        .domain(['e', 's', 'w', 'na'])
+    poScale
+        .domain(types)
         .range([10, height/2])
 
-    maxRad = d3.max(freq);
-    minRad = d3.min(freq);
-    radScale.domain([minRad, maxRad])
-
     simulation = d3.forceSimulation()
-        .force('link', d3.forceLink().id(d => d.id).strength(0.9)
-            .distance(60).strength(0.3))
-        .force('charge', d3.forceManyBody(-100))
-        .force('collide', d3.forceCollide().radius(radius*20).strength(0.9))
+        .force('link', d3.forceLink().id(d => d.id).strength(0.01)
+            .distance(height/2).strength(0.3))
+        // .force('charge', d3.forceManyBody(-100))
+        // .force('collide', d3.forceCollide().radius(radius*2).strength(0.1))
         // .force('r', d3.forceRadial(function(d){
-        //     return poScale(d.loc) //nodes are placed in relation to their halls
-        // }).strength(0.3)) //.001
+        //     return poScale(d.type) //nodes are placed in relation to their halls
+        // }).strength(0.9)) //.001
    
     svg = d3.select('body').append('svg')
         .attr('viewBox', [-width/2,-height/2, width, height]);
@@ -76,12 +76,15 @@ const processPrep = async(dataset, nodes) => {
         .selectAll('text');
     link = svg.append('g')
         .selectAll('line');
-    //to trigger progression - this could be time based, for example
     svg.on("click", function(){ 
         series();
     })
 
     return svg;
+}
+
+function zoomed({ transform }) {
+    svg.attr('transform', transform)
 }
 
 function series(){
@@ -92,18 +95,21 @@ function series(){
 function chooseData(whichNum){
     console.log(whichNum)
 
-    //if the index of whichnum matches the question id, let's see those nodes - 
-    //so we are progressively adding in q+a to the network
     for (var i = 0; i<nodes.length; i++){
-        if(nodes[i].questionID == whichNum){  
+        if(nodes[i].type == whichNum){  
             liveNodes.push(nodes[i])
+        }
+    }
+    for (var i = 0; i<topicNodes.length; i++){
+        if(topicNodes[i].type == whichNum){  
+            liveTopics.push(topicNodes[i])
         }
     }
 
     for (var i = 0; i<liveNodes.length; i++){
         for(j=0; j<liveNodes[i].tags.length; j++){
             for (k=0; k<topicNodes.length; k++){
-                if(liveNodes[i].tags[j]==topicNodes[k].tags && liveNodes[i].questionID == whichNum){
+                if(liveNodes[i].tags[j]==topicNodes[k].tags && liveNodes[i].type == whichNum){
                     liveLinks.push({
                         "source": liveNodes[i].id,
                         "target": topicNodes[k].id,
@@ -131,72 +137,84 @@ const yellow = 255;
 function restart(liveLinks, liveNodes, whichNum){
     console.log('restart')
 
+    // node = node
+    //     .data(liveNodes, function(d){
+    //         return d.id;
+    //     })
+    // node.exit()
+    //     .remove();
+    // node = node.enter().append('circle')
+    //     .attr('r',function(d){
+    //         return radius; //to be changed to be in accordance with is this first or not
+    //     })
+    //     .attr('fill',function(d){
+    //         if(d.tags.length>1){
+    //             return 'pink'
+    //         }else{
+    //             return 'white'; //to be changed to be in accordance with is this first or not  
+    //         }
+    //     })
+    //     .attr('opacity',.5)
+    //     .merge(node);
     node = node
         .data(liveNodes, function(d){
             return d.id;
         })
     node.exit()
         .remove();
-    node = node.enter().append('circle')
-        .attr('r',function(d){
-            if(d.answer==-1){
-                return radScale(d.frequency);
-            }else {
-                return radius*2;
-            }
+
+    node = node.enter().append('image')
+        .attr('class', function(d){
+            return 'n'+d.id;
         })
-        .attr('fill',function(d){
-            if(d.answer==-1){
-                //no color scale for main topic tags
-                return 'none' 
-            }else {
-                if(d.loc=='e'){
-                    if(d.type=='scale'){
-                        var sat = (satScale(d.answer)).toString()
-                        return d3.lab('hsl('+yellow+','+sat+'%, 100%)')
-                    }
-                    if(d.type=='scale-cat'){
-                        var sat = (satScale(d.answer*2)).toString()
-                        return d3.lab('hsl('+yellow+','+sat+'%, 100%)')
-                    }
-                    else{
-                        return d3.lab('hsl('+yellow+',100%, 100%)')
-                    }
-                }
-            }
+        .attr("xlink:href", function(d){
+            var max = 8;
+            var initialRandom = Math.random();
+            var multiplied = initialRandom * max;
+            var answer = Math.floor(multiplied);
+
+            return 'symb/clouds/'+d.symb+'/'+answer+'.png';
         })
-        .attr('stroke', function(d){
-            if(d.answer==-1){
-                return 'white'
-            }
-            else {
-                if(d.loc=='e'){
-                    if(d.type=='scale'){
-                        var sat = (satScale(d.answer)).toString()
-                        return d3.lab('hsl('+yellow+','+sat+'%, 100%)')
-                    }
-                    if(d.type=='scale-cat'){
-                        var sat = (satScale(d.answer*2)).toString()
-                        return d3.lab('hsl('+yellow+','+sat+'%, 100%)')
-                    }
-                    else{
-                        return d3.lab('hsl('+yellow+',100%, 100%)')
-                    }
-                }
-            }
-        })
-        .attr('stroke-width', 1)
-        .attr('stroke-opacity', function(d){
-            // if(d.answer==-1){
-                return opacityScale(d.frequency);
-            // }else{
-                // return 1;
-            // }
-        })
+        .attr("transform","translate("+ -symWidth/2 +","+ -symHeight/2 +")")
+        .attr("width", symWidth+"px")
+        .attr("height", symHeight+"px")
         .merge(node);
 
+
+    // img = img
+    //     .data(liveNodes, function(d){
+    //         return d;
+    //     })
+    //     .attr("opacity", function (d){
+    //             if(d.type==whichNum){
+    //                 return .4;
+    //             }else{
+    //                 return .2;
+    //             }
+    //     })
+    // img.exit()
+    //     .remove();
+    // img = img.enter()
+    //     .filter(function(d) { 
+    //         if(d.img!=undefined && d.first ==1){
+    //             if(d.type==whichNum){
+    //                 return d;
+    //             }
+    //         }
+    //     })
+    //     .append("svg:image")
+    //     .attr("xlink:href", function(d) {
+    //         return d.img;
+    //     })
+    //     .attr("x", -width/2)
+    //     .attr("y", -100)
+    //     .attr("width", 1200+'px')
+    //     .attr("height", 1000+'px')
+    //     .attr("opacity",.4)
+    //     .merge(img);
+
     text = text
-        .data(topicNodes, function(d){
+        .data(liveTopics, function(d){
             return d.id
         })
     text.exit()
@@ -206,12 +224,19 @@ function restart(liveLinks, liveNodes, whichNum){
         .append('text')
         .attr('dy', '.31em') 
         .attr('dx', '.41em') 
-        .attr('font-size','8px')
+        .attr('font-size','1px')
         .attr('fill','white')
         .text(function(d){
             for(i=0; i<tagTable.length; i++){
                 if(d.tags==tagTable[i].tagID){
                     return tagTable[i].tag.toUpperCase();
+                }
+                if(d.tags.length>1){
+                    for(j=0; j<d.tags.length; j++){
+                        if(d.tags[j] == tagTable[i].tagID){
+                            return tagTable[i].tag.toUpperCase();                            
+                        }
+                    }
                 }
             }
         })
@@ -230,7 +255,7 @@ function restart(liveLinks, liveNodes, whichNum){
         .attr('stroke','white')
         .attr('stroke-width',.1)
         .attr('fill','white')
-        .attr('fill-opacity',.001)
+        .attr('fill-opacity',.09)
         .merge(link);
 
 
@@ -262,7 +287,7 @@ function restart(liveLinks, liveNodes, whichNum){
 function ticked() {
     node.attr('class', positionNodes);
     link.attr('d', makeLinks);
-    text.attr('x', positionNodes)
+    text.attr('class', positionNodes)
 }
 
 function positionNodes(d) {
@@ -270,14 +295,17 @@ function positionNodes(d) {
         .attr('class', function(d){
             return 'n'+d.id;
         })
-        .attr('cy', function(d) { 
+        .attr('y', function(d) { 
             return d.y;  
         })
-        .attr('cx', function(d) {
+        .attr('x', function(d) {
             return d.x; 
         })
     
     text
+        .attr('class', function(d){
+            return 't'+d.id;
+        })
         .attr('y', function(d) { 
             return d.y;  
         })
